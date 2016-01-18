db6f844f1c10ad9934f2574d7d66a838689ee463
splitice <mat999@gmail.com>
2016-01-18 20:16:30 +1100

    improve X509 classes

---
+++ src/Radical/Utility/SSL/X509Certificate.php
@@ -0,0 +1,59 @@
+<?php
+namespace Radical\Utility\SSL;
+
+
+use phpseclib\File\X509;
+
+class X509Certificate
+{
+	private $x509;
+	private $cert_data;
+
+	function __construct($data, X509 $x509 = null)
+	{
+		if($x509) {
+			$this->x509 = clone $x509;
+		}else{
+			$this->x509 = new X509();
+		}
+
+		$this->cert_data = $this->x509->loadX509($data);
+	}
+
+
+
+	function getSubject(){
+		return $this->cert_data['tbsCertificate']['subject'];
+	}
+
+	public function getExtension($name){
+		foreach ($this->cert_data['tbsCertificate']['extensions'] as $extension) {
+			if ($extension['extnId'] == $name){
+				return $extension['extnValue'];
+			}
+		}
+
+	}
+
+	function isSigned(){
+		return $this->x509->validateSignature();
+	}
+
+	/**
+	 * Get the URL of the parent certificate.
+	 *
+	 * @return string
+	 */
+	public function getParentCertificateURL()
+	{
+		foreach ($this->getExtension('id-pe-authorityInfoAccess') as $extnValue) {
+			if ($extnValue['accessMethod'] == 'id-ad-caIssuers') {
+				return $extnValue['accessLocation']['uniformResourceIdentifier'];
+			}
+		}
+	}
+
+	function getContents(){
+		return $this->x509->saveX509($this->cert_data)."\r\n";
+	}
+}
---
+++ src/Radical/Utility/SSL/X509Helpers.php
@@ -0,0 +1,51 @@
+<?php
+namespace Radical\Utility\SSL;
+
+class X509Helpers {
+	function checkPair($cert, $key, $passphrase = null){
+		if(openssl_pkey_get_private($key,$passphrase) === false){
+			return false;
+		}
+		return openssl_x509_check_private_key($cert,$key);
+	}
+	
+	function generatePrivateKey(){
+		return openssl_pkey_new();
+	}
+	
+	function generate(SigningDetails $dn, $privateKey = null, $privkeypass = null, $numberofdays = 365){
+		if($privateKey === null){
+			$privkey = $this->generatePrivateKey();
+		}elseif(is_string($privateKey)){
+			$privkey = openssl_pkey_get_private($privateKey);
+		}else{
+			throw new \Exception('Invalid format for private key');
+		}
+		
+		if(!$privkey){
+			throw new \Exception('Invalid private key');
+		}
+		
+		$csr = @openssl_csr_new($dn->toArray(), $privkey);
+        if(!$csr){
+            throw new \Exception('Failed create signing request. Input likely invalid.');
+        }
+
+		$sscert = openssl_csr_sign($csr, null, $privkey, $numberofdays);
+        if(!$sscert){
+            throw new \Exception('Failed create signing request. Input likely invalid.');
+        }
+
+		openssl_x509_export($sscert, $publickey);
+		$privatekey = null;
+		if(!openssl_pkey_export($privkey, $privatekey, $privkeypass)){
+			throw new \Exception('Private key generatio failed');
+		}
+		/*$csrStr = null;
+		if(!openssl_csr_export($csr, $csrStr)){
+			throw new \Exception('CSR generation failed');
+		}*/
+		
+		return new X509CertificatePair($publickey, $privatekey);
+	}
+}
